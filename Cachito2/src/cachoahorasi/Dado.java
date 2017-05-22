3249729121ad73e619c69dd51964133d9c43f747
jict3493 <jict3493@gmail.com>
2017-05-21 23:44:46 -0400

    pequeño cambio, garantiza mas random

--- Cachito2/src/cachoahorasi/Dado.java
+++ Cachito2/src/cachoahorasi/Dado.java
@@ -4,15 +4,15 @@
  * and open the template in the editor.
  */
 package cachoahorasi;
-
+import java.security.SecureRandom; //Ofrece mayor seguridad de que sera al azar
 /**
  *
  * @author pabloneitor
  */
 public class Dado {
     public int lanzarDado(){
-        int dado;
-        dado = (int)(Math.random()*6)+1;
+        SecureRandom sr = new SecureRandom();
+        int dado = sr.nextInt(6)+1;
         return dado;
     }
     public int voltearDado(int n){
